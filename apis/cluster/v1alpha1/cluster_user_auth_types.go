6ac2f22cc05215c9b9cb635c1684eafee0d8a089
Masudur Rahman <masud@appscode.com>
2023-05-31 16:24:12 +0600

    Add CredentialName field in ClusterUserAuth spec
    Signed-off-by: Masudur Rahman <masud@appscode.com>

--- apis/cluster/v1alpha1/cluster_user_auth_types.go
+++ apis/cluster/v1alpha1/cluster_user_auth_types.go
@@ -84,9 +84,15 @@ type ClusterUserAuthSpec struct {
 
 	// Provider Access Token params
 	// +optional
-	Provider    TokenProviderName    `json:"provider,omitempty" protobuf:"bytes,12,opt,name=provider,casttype=TokenProviderName"`
-	GoogleOAuth *GoogleOAuthProvider `json:"googleOAuth,omitempty" protobuf:"bytes,13,opt,name=googleOAuth"`
-	AWS         *AWSProvider         `json:"aws,omitempty" protobuf:"bytes,14,opt,name=aws"`
+	Provider       TokenProviderName    `json:"provider,omitempty" protobuf:"bytes,12,opt,name=provider,casttype=TokenProviderName"`
+	GoogleOAuth    *GoogleOAuthProvider `json:"googleOAuth,omitempty" protobuf:"bytes,13,opt,name=googleOAuth"`
+	AWS            *AWSProvider         `json:"aws,omitempty" protobuf:"bytes,14,opt,name=aws"`
+	CredentialName string               `json:"credentialName,omitempty" protobuf:"bytes,15,opt,name=credentialName"`
+}
+
+type GoogleCloudCredential struct {
+	ProjectID      string `json:"projectID" protobuf:"bytes,1,opt,name=projectID"`
+	ServiceAccount string `json:"serviceAccount" protobuf:"bytes,2,opt,name=serviceAccount"`
 }
 
 type GoogleOAuthProvider struct {
