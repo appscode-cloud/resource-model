9a909ee196fdd76ad3359c32a03bf7bc5c67b629
Tamal Saha <tamal@appscode.com>
2025-12-30 11:15:41 +0600

    Use k8s 1.34 client libs
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/cloud/fuzzer/fuzzer.go
+++ apis/cloud/fuzzer/fuzzer.go
@@ -20,21 +20,21 @@ package fuzzer
 import (
 	v1 "go.bytebuilders.dev/resource-model/apis/cloud/v1alpha1"
 
-	fuzz "github.com/google/gofuzz"
 	runtimeserializer "k8s.io/apimachinery/pkg/runtime/serializer"
+	"sigs.k8s.io/randfill"
 )
 
 // Funcs returns the fuzzer functions for this api group.
 var Funcs = func(codecs runtimeserializer.CodecFactory) []any {
 	return []any{
-		func(s *v1.CloudProvider, c fuzz.Continue) {
-			c.FuzzNoCustom(s) // fuzz self without calling this function again
+		func(s *v1.CloudProvider, c randfill.Continue) {
+			c.Fill(s) // fuzz self without calling this function again
 		},
-		func(s *v1.Credential, c fuzz.Continue) {
-			c.FuzzNoCustom(s) // fuzz self without calling this function again
+		func(s *v1.Credential, c randfill.Continue) {
+			c.Fill(s) // fuzz self without calling this function again
 		},
-		func(s *v1.MachineType, c fuzz.Continue) {
-			c.FuzzNoCustom(s) // fuzz self without calling this function again
+		func(s *v1.MachineType, c randfill.Continue) {
+			c.Fill(s) // fuzz self without calling this function again
 		},
 	}
 }
--- apis/config/fuzzer/fuzzer.go
+++ apis/config/fuzzer/fuzzer.go
@@ -20,15 +20,15 @@ package fuzzer
 import (
 	"go.bytebuilders.dev/resource-model/apis/config/v1alpha1"
 
-	fuzz "github.com/google/gofuzz"
 	runtimeserializer "k8s.io/apimachinery/pkg/runtime/serializer"
+	"sigs.k8s.io/randfill"
 )
 
 // Funcs returns the fuzzer functions for this api group.
 var Funcs = func(codecs runtimeserializer.CodecFactory) []any {
 	return []any{
-		func(s *v1alpha1.AceSetupConfig, c fuzz.Continue) {
-			c.FuzzNoCustom(s) // fuzz self without calling this function again
+		func(s *v1alpha1.AceSetupConfig, c randfill.Continue) {
+			c.Fill(s) // fuzz self without calling this function again
 		},
 	}
 }
